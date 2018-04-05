77a9c34bfc3c2e32bfb3beb7c48887dec349a0ef
Joost De Cock <joost@decock.org>
2018-04-05 18:34:42 +0200

    Progress on admin add admin page

--- pages/admin/add/admins.vue
+++ pages/admin/add/admins.vue
@@ -1,30 +1,34 @@
 <template>
-  <section class="container text-xs-center">
-    <h1>{{ $t('mainMenu.admin') }}</h1>
-    <div v-if="$auth.loggedIn">
-      <p>You are logged in</p>
-      <pre>{{ $auth.user }} </pre>
-      <v-btn @click="logout" color="secondary" large>Logout</v-btn>
-    </div>
-    <div v-else>
-      <h4>{{ $t('admin.loginBelow') }}</h4>
+  <section class="container">
+    <h1>{{ $t('adminMenu.addAdmins') }}</h1>
+    <div v-if="$auth.user.isSuperadmin">
+     <h4>{{ $t('admin.loginBelow') }}</h4>
       <v-form v-model="valid">
         <v-text-field
-          :label="$t('admin.username')"
+          :label="$t('admin.add.admins.username')"
           v-model="username"
           required
         ></v-text-field>
         <v-text-field
-          :label="$t('admin.password')"
+          :label="$t('admin.add.admins.password')"
           v-model="password"
-          type="password"
           required
         ></v-text-field>
         <v-btn @click="submit" color="primary" large>
-          {{ $t('mainMenu.login') }}
+          {{ $t('admin.add.admins.submit') }}
           <v-progress-circular indeterminate color="#fff" class="ml-4" v-if="loading" :size="20" :width="2"></v-progress-circular>
         </v-btn>
+        <v-btn @click="randomPassword" large>
+          {{ $t('admin.add.admins.randomPassword') }}
+        </v-btn>
       </v-form>
+      <pre>{{ $auth.user }} </pre>
+    </div>
+    <div v-else>
+    <blockquote class="error mt-5 dark">
+      <h3 class="white-text">{{ $t('admin.accessDenied') }}</h3>
+      <p class="white-text">{{ $t('admin.superadminOnly') }}</p>
+    </blockquote>
     </div>
     <v-snackbar
       :timeout="(4000)"
@@ -74,9 +78,8 @@ export default {
         this.loading = false;
       })
     },
-    logout: function() {
-      this.$auth.logout()
-      this.$auth.reset()
+    randomPassword: function() {
+      console.log('do something')
     }
   }
 }
@@ -84,7 +87,8 @@ export default {
 
 <style scoped>
   form {
-    max-width: 300px;
-    margin: auto;
+  }
+  .white-text {
+    color: #fff;
   }
 </style>
